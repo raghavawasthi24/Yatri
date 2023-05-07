4f2fd102f5103d55756a5703ec7c50659e6e1f12
N03Nilay <rudranilay03@gmail.com>
2023-05-07 10:39:00 +0530

    added file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-// import { useEffect } from 'react';
+ import { useEffect } from 'react';
 import './App.css';
-// import axios from 'axios';
+ import axios from 'axios';
 
 function App() {
 
-  // useEffect(()=>{
-  //   axios.get("https://pnr-status-indian-railway.p.rapidapi.com/pnr-check/6719115281",{
-  //     method: 'GET',
-  //     url: 'https://pnr-status-indian-railway.p.rapidapi.com/pnr-check/8531575878',
-  //     headers: {
-  //       'X-RapidAPI-Key': '4fb6eff0admshbdf5ceb06511570p152d5djsne0fb02731d5f',
-  //       'X-RapidAPI-Host': 'pnr-status-indian-railway.p.rapidapi.com'
-  //     }
-  //   })
-  //   .then((res)=>{
-  //     console.log(res)
-  //   }).catch((err)=>{
-  //     console.log(err)
-  //   })
-  //  })
+  useEffect(()=>{
+    axios.get("https://pnr-status-indian-railway.p.rapidapi.com/pnr-check/6719115281",{
+      method: 'GET',
+      url: 'https://pnr-status-indian-railway.p.rapidapi.com/pnr-check/8531575878',
+      headers: {
+        'X-RapidAPI-Key': '4fb6eff0admshbdf5ceb06511570p152d5djsne0fb02731d5f',
+        'X-RapidAPI-Host': 'pnr-status-indian-railway.p.rapidapi.com'
+      }
+    })
+    .then((res)=>{
+      console.log(res)
+    }).catch((err)=>{
+      console.log(err)
+    })
+   })
 
   // useEffect(()=>{
   //   axios.get("https://irctc1.p.rapidapi.com/api/v3/trainBetweenStations",{
